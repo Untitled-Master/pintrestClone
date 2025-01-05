f1463c04a4bdb7ec958863596375e50e57a6b81a
Ahmed Belmehnouf <a_belmehnouf@estin.dz>
2025-01-05 22:53:04 +0100

    Your commit message here

--- pintrestclone/src/app/components/Header.tsx
+++ pintrestclone/src/app/components/Header.tsx
@@ -1,10 +1,14 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import { HiSearch,HiBell,HiChat } from "react-icons/hi"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 function Header() {
+  const { data: session } = useSession();
+  console.log(session)
   return (
-    <div className='flex gap-3 items-center p-6'>
+    <div className='flex gap-3 md:gap-2 items-center p-6'>
         <Image src='/logo.png' alt='logo' width={50} height={50}
         className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
         <button className='bg-black text-white p-2 px-4 rounded-full'>Home</button>
@@ -15,8 +19,9 @@ function Header() {
         </div>
         <HiBell className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>
         <HiChat className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>  
-        <Image src='/man.png' alt='user image' width={50} height={50}
-        className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
+        {session?.user? <Image src={session?.user?.image} alt='user image' width={50} height={50}
+        className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>:
+        <button className='font-semibold p-2 px-4 rounded-full' onClick={() => signIn()}>Login</button>}
     </div>
   )
 }

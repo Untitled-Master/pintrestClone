5e5f1d5bfd79d13a3c356eaddb01e5547f9adb07
Ahmed Belmehnouf <a_belmehnouf@estin.dz>
2025-01-08 16:50:22 +0100

    fixed Provider children

--- pintrestclone/src/app/Provider.tsx
+++ pintrestclone/src/app/Provider.tsx
@@ -1,13 +1,17 @@
-"use client"
-import React from 'react'
-import { SessionProvider } from "next-auth/react"
+"use client";
+import React, { ReactNode } from 'react';
+import { SessionProvider } from "next-auth/react";
 
-function Provider({children}) {
-  return (
-    <SessionProvider >
-        {children}
-    </SessionProvider>
-  )
+interface ProviderProps {
+  children: ReactNode;
 }
 
-export default Provider
+function Provider({ children }: ProviderProps) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  );
+}
+
+export default Provider;

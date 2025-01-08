6bd8519d4ba15f0290d3b2aaf2c3da16b7ce132a
Ahmed Belmehnouf <a_belmehnouf@estin.dz>
2025-01-08 16:53:06 +0100

    fixed Header.tsx

--- pintrestclone/src/app/components/Header.tsx
+++ pintrestclone/src/app/components/Header.tsx
@@ -1,27 +1,38 @@
 "use client"
 import React from 'react'
 import Image from 'next/image'
-import { HiSearch,HiBell,HiChat } from "react-icons/hi"
+import { HiSearch, HiBell, HiChat } from "react-icons/hi"
 import { useSession, signIn } from "next-auth/react"
-/*add signOut later */
+
 function Header() {
   const { data: session } = useSession();
-  console.log(session)
+  console.log(session);
+
+  const userImage = session?.user?.image || '/default-user.png'; // Fallback image
+
   return (
     <div className='flex gap-3 md:gap-2 items-center p-6'>
-        <Image src='/logo.png' alt='logo' width={50} height={50}
+      <Image src='/logo.png' alt='logo' width={50} height={50}
         className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
-        <button className='bg-black text-white p-2 px-4 rounded-full'>Home</button>
-        <button className='font-semibold p-2 px-4 rounded-full'>Create</button>
-        <div className='bg-[#e9e9e9] p-3 flex gap-3 items-center rounded-full w-full'>
-            <HiSearch className='text-[25px] text-gray-500'/>
-            <input type="text" placeholder='search...' className='bg-transparent outline-none'/>
-        </div>
-        <HiBell className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>
-        <HiChat className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>  
-        {session?.user? <Image src={session?.user?.image} alt='user image' width={50} height={50}
-        className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>:
-        <button className='font-semibold p-2 px-4 rounded-full' onClick={() => signIn()}>Login</button>}
+      <button className='bg-black text-white p-2 px-4 rounded-full'>Home</button>
+      <button className='font-semibold p-2 px-4 rounded-full'>Create</button>
+      <div className='bg-[#e9e9e9] p-3 flex gap-3 items-center rounded-full w-full'>
+        <HiSearch className='text-[25px] text-gray-500'/>
+        <input type="text" placeholder='search...' className='bg-transparent outline-none'/>
+      </div>
+      <HiBell className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>
+      <HiChat className='text-[25px] md:text-[60px] text-gray-500 cursor-pointer'/>  
+      {session?.user ? (
+        <Image 
+          src={userImage as string} 
+          alt='user image' 
+          width={50} 
+          height={50}
+          className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'
+        />
+      ) : (
+        <button className='font-semibold p-2 px-4 rounded-full' onClick={() => signIn()}>Login</button>
+      )}
     </div>
   )
 }
